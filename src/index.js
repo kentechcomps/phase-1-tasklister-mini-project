61c577549eaad928dc14886d68831ddd7e876ab7
user <mutukukennedy34@gmail.com>
2023-06-22 15:47:57 +0300

    todolst

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  let descrpt = document.getElementById('new-task-description')
+  let formbtn = document.getElementById('submiit')
+  let uls = document.getElementById('tasks')
+  
+  formbtn.addEventListener("submit" , ()=> {
+      
+      let namelist = document.createElement('li')
+      namelist.innerText = descrpt.value
+      uls.appendChild(namelist)
+      e.preventDefault(n)
+   })
+   
